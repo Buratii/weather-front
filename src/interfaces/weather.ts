8c94cd944a9a4817ed6a0941e1df4876ee05498b
Leonardo Burati <leonardoburati@hotmail.com>
2023-03-14 14:25:15 -0300

    feat: create weather interface

--- src/interfaces/weather.ts
+++ src/interfaces/weather.ts
@@ -17,12 +17,28 @@ export interface Location {
 
 export interface Current {
   condition: Condition;
+  feelslike_c: number;
+  feelslike_f: number;
+  gust_mph: number;
+  gust_kph: number;
+  cloud: number;
+  humidity: number;
   is_day: number;
+  precip_in: number;
+  precip_mm: number;
+  pressure_in: number;
+  pressure_mb: number;
   last_updated: string;
   last_updated_epoch: number;
   temp_c: number;
   temp_f: number;
   uv: number;
+  vis_km: number;
+  vis_miles: number;
+  wind_degree: number;
+  wind_dir: string;
+  wind_kph: number;
+  wind_mph: number;
 }
 
 export interface Weather {
