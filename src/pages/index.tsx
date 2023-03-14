bf85f066de7c2299f8cd3ddaebbbfa6c386c3459
Leonardo Burati <leonardoburati@hotmail.com>
2023-03-14 14:26:07 -0300

    feat: create searching method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,25 +3,45 @@ import { Weather } from "@/interfaces/weather";
 import api from "@/services/api";
 import { Flex, HStack, Img, Stack, Text } from "@chakra-ui/react";
 import { format } from "date-fns";
-import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
+import { useState } from "react";
 
 interface HomeProps {
-  weather: Weather;
+  initialWeather: Weather;
 }
 
-export default function Home({ weather }: HomeProps) {
-  const [search, setSearch] = useState("Criciuma");
+export default function Home({ initialWeather }: HomeProps) {
+  const [weather, setWeather] = useState<Weather>(initialWeather);
   const [historySearch, setHistorySearch] = useState<string[]>([]);
 
-  useEffect(() => {
-    const getWeather = async () => {
+  const getWeather = async (search: string) => {
+    try {
+      const query = search.normalize("NFD").replace(/\p{Diacritic}/gu, "");
+
+      const alreadySearched = historySearch.find((location) =>
+        location.match(query)
+      );
+
       const { data } = await api.get<Weather>(`&q=${search}`);
 
-      setHistorySearch([...historySearch, data.location.name]);
-    };
+      setWeather(data);
 
-    getWeather();
-  }, [search]);
+      const local = data.location.name
+        .normalize("NFD")
+        .replace(/\p{Diacritic}/gu, "");
+
+      if (!alreadySearched) {
+        setHistorySearch([...historySearch, local]);
+
+        if (historySearch.length === 4) {
+          historySearch.shift();
+          setHistorySearch([...historySearch, local]);
+        }
+      }
+    } catch (error) {
+      throw new Error("Unable to consume api.");
+    }
+  };
 
   return (
     <Flex
@@ -33,26 +53,42 @@ export default function Home({ weather }: HomeProps) {
     >
       <HStack position="absolute" bottom={24} left={24} userSelect="none">
         <Text fontSize={164} fontWeight="medium">
-          {Math.round(weather.current.temp_c)}°
+          {Math.round(weather?.current.temp_c)}°
         </Text>
         <Flex>
           <Stack>
-            <Text fontSize={64}>{weather.location.name}</Text>
+            <Text fontSize={64}>{weather?.location.name}</Text>
             <Text>
               {format(
-                new Date(weather.location.localtime),
+                new Date(weather?.location.localtime),
                 "HH:mm - EEEE, dd MMM yy"
               )}
             </Text>
           </Stack>
-          <Stack align="center" spacing={4} justify="flex-end">
-            <Img src={weather.current.condition.icon} />
-            <Text>{weather.current.condition.text}</Text>
+          <Stack align="center" mx="4" spacing={4} justify="flex-end">
+            <Img src={weather?.current.condition.icon} />
+            <Text>{weather?.current.condition.text}</Text>
           </Stack>
         </Flex>
       </HStack>
 
-      <Sidebar />
+      <Sidebar search={getWeather} history={historySearch} weather={weather} />
     </Flex>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  initialWeather: Weather;
+}> = async (context) => {
+  const { data } = await api.get<Weather>(`&q=Criciuma`, {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+
+  return {
+    props: {
+      initialWeather: data,
+    },
+  };
+};
